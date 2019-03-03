44e2a736965c33954481d5eae41818580941ee68
Amal Jossy <amaljossy1@gmail.com>
2019-03-04 02:00:48 +0530

    Category model auth

--- application/models/Category_model.php
+++ application/models/Category_model.php
@@ -1,23 +1,64 @@
 <?php
 class Category_model extends CI_Model {
+    public function get_categories($id){
+        $this->db->select('cat_id, cat_name');
+        if( $id != NULL ){
+            $this->db->where('cat_id', $id );
+        }
+        $query = $this->db->get('categories');
+        return $query->result_array();
+    }
     public function create($data){
+        if( $this->session->type != 'super' ){
+            return 401;
+        }
         $data['pswd']=password_hash($data['pswd'],PASSWORD_BCRYPT);
         $this->db->insert('categories', $data);
+        return 201;
     }
     public function modify($id,$data){
-        foreach($data as $attribute => $value){
-            if( $value != NULL ){
-                $this->db->set($attribute, $value);
-            }
-            if( $key == 'pswd' ){
-                $this->db->set($key,password_hash($value,PASSWORD_BCRYPT));
+        if( $this->session->type != 'super' ){
+            return 401;
+        }
+        foreach($data as $key => $value){
+            if( $value != NULL ){ 
+                if( $key == 'pswd' ){
+                    $this->db->set($key,password_hash($value,PASSWORD_BCRYPT));
+                }
+                else{
+                    $this->db->set($key, $value);
+                }
             }
         }
-        $this->db->where('cat_id', $data['cat_id']);
+        $this->db->where('cat_id', $id);
         $this->db->update('categories');
+        return 200;
     }
     public function delete($id){
-        $this->db->delete('categories', array('id' => $id));
+        if( $this->session->type != 'super' ){
+            return 401;
+        }
+        $this->db->delete('categories', array('cat_id' => $id));
+        return 200;
+    }
+    public function validate($username,$pswd){
+        $query = $this->db->get_where('categories', array('username' => $username));
+        if($query->num_rows() == 1){
+            $row = $query->row();
+            $hash = $row->pswd;
+            if(password_verify($pswd,$hash)){
+                $data = array(
+                    'type' => 'category',
+                    'cat_id' => $row->cat_id,
+                    'cat_name' => $row->cat_name,
+                    'username' => $row->username,
+                    'validated' => true
+                );
+                $this->session->set_userdata($data);
+                return true;
+            }
+        }
+        return false;
     }
 }
 ?>
