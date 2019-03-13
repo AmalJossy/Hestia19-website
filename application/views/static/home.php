b71789401fb77d781f0498358024a0d6f5759819
muhammedshaheer <ckshaheer2012@gmail.com>
2019-03-13 15:22:38 +0530

    updated title again

--- application/views/static/home.php
+++ application/views/static/home.php
@@ -21,7 +21,7 @@
 
 
   <title>
-    Hestia 19 - National Level TechnoCultural Fest of TKM
+    Hestia 19 - National Level Techno-Cultural Fest of TKM
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
