340f1ce01141912c585334904eda7ce5801a37b9
Nameer PK <nameerpk.95@gmail.com>
2019-03-21 10:48:56 +0530

    auth to event registration

--- application/models/Registration_model.php
+++ application/models/Registration_model.php
@@ -4,8 +4,14 @@ class Registration_model extends CI_Model {
         if( $this->session->type != 'super' && $this->session->event_id != $id){
             return 401;
         }
-        $query = $this->db->get_where('registrations',array('event_id' => $id));
+        $this->db->select('reg_id, event_id, reg_email, member_email');
+
+        $this->db->where('event_id', $id );
+
+        $query = $this->db->get('registrations');
         return $query->result_array();
+        // $query = $this->db->get_where('registrations',array('event_id' => $id));
+        // return $query->result_array();
     }
     public function get_registrations($id){
         $this->db->select('reg_id, event_id, reg_email, member_email');
