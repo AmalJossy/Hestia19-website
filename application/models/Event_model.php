e2968f25c8f6195c173c91406b0bf09aedcea565
Nameer PK <nameerpk.95@gmail.com>
2019-04-09 16:27:45 +0530

    certificate enabling code added

--- application/models/Event_model.php
+++ application/models/Event_model.php
@@ -3,7 +3,7 @@ class Event_model extends CI_Model {
     public function get_category_events($id){
         $this->db->select('event_id, cat_id, title, short_desc, details, min_memb, max_memb,
                     venue, reg_fee, fee_type, prize, file1, file2, co1_name,
-                    co1_no, co2_name, co2_no, seats, reg_start, reg_end, link');
+                    co1_no, co2_name, co2_no, seats, reg_start, reg_end, link, is_certificate_pub');
 
         $this->db->where('cat_id', $id );
 
@@ -13,12 +13,21 @@ class Event_model extends CI_Model {
     public function get_event($id){
         $this->db->select('event_id, cat_id, title, short_desc, details, min_memb, max_memb,
                     venue, reg_fee, fee_type, prize, file1, file2, file_last_date, co1_name,
-                    co1_no, co2_name, co2_no, seats, reg_start, reg_end, username, link');
+                    co1_no, co2_name, co2_no, seats, reg_start, reg_end, username, link, is_certificate_pub');
         if( $id != NULL ){
             $this->db->where('event_id', $id );
         }
         $query = $this->db->get('events');
         return $query->result_array();
+    }
+    public function enable_certificate($eid) {
+        if( $this->session->type != 'super' && $this->session->event_id != $eid){
+            return 401;
+        }
+        $this->db->set('is_certificate_pub', 1);
+        $this->db->where('event_id', $eid);
+        $this->db->update('events');
+        return 200;
     }
     
     public function get_event_by_link($link){
@@ -32,7 +41,7 @@ class Event_model extends CI_Model {
         return $query->result_array();
     }
     public function create($data){
-        if( $this->session->type != 'super' OR $this->session->cat_id != $data['cat_id'] ){
+        if( $this->session->type != 'super' && $this->session->cat_id != $data['cat_id'] ){
             return 401;
         }
         $data['pswd']=password_hash($data['pswd'],PASSWORD_BCRYPT);
